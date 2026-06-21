c35c113fcb25fc8bbc37dfc1421aec51b450d73c
Phanty78 <maeldonnart@hotmail.fr>
2026-06-21 20:52:37 +0200

    ADD - ajout de la methode POST pour spot.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import type { Spot } from "./types.ts"
+import type { Spot, SpotError } from "./types.ts"
 
 const spots: Spot[] = [
   {
@@ -37,11 +37,79 @@ const server = Bun.serve({
         },
 
         "/spots": {
-            GET: () => Response.json(spots)
+            GET: () => Response.json(spots),
+            POST: async req => {
+              try {
+                const body = await req.json()
+                const parsedBody = validateBody(body)
+                if ("ok" in parsedBody){
+                  return new Response (`${parsedBody.message}`, { status: 400 })
+                }
+                spots.push(parsedBody)
+                return new Response ("new spot added", { status : 201 })
+              } catch {
+                return new Response ("invalid JSON", { status : 400 })
+              }
+            },
         },
 
         "/*": new Response ("Lost in the forest?", { status : 404 })
     }
 })
+
+function validateBody(body : unknown): Spot | SpotError{
+    if (typeof body !== "object" || body === null){
+        return { ok : false, message : "invalid body" }
+    }
+    if (!("title" in body)){
+        return { ok : false, message : "title missing" }
+    }
+    if (!("category" in body)){
+        return { ok : false, message : "category missing" }
+    }
+    if (!("latitude" in body)){
+        return { ok : false, message : "latitude missing" }
+    }
+    if (!("longitude" in body)){
+        return { ok : false, message : "longitude missing" }
+    }
+
+    if (typeof body.title !== "string"){
+      return { ok : false, message : "title invalid" }
+    }
+
+    let description: string | null = null
+
+    if ("description" in body) {
+      if (body.description !== null && typeof body.description !== "string") {
+        return { ok: false, message: "description invalid" }
+      }
+
+    description = body.description
+    }
+
+    const latitudeNumber = Number(body.latitude)
+    const longitudeNumber = Number(body.longitude)
+    if (Number.isNaN(latitudeNumber)){
+        return { ok : false, message : "latitude invalid" }
+    }
+    if (Number.isNaN(longitudeNumber)){
+        return { ok : false, message : "longitude invalid" }
+    }
+
+    if (body.category === "viewpoint" || body.category === "danger" || body.category === "water" || body.category === "parking" || body.category === "other") {
+      return {
+        id: crypto.randomUUID(),
+        title: body.title,
+        description: description,
+        category: body.category,
+        latitude: latitudeNumber,
+        longitude: longitudeNumber,
+        createdAt: new Date().toISOString()
+      }
+    } else {
+      return { ok : false, message : "category invalid" }
+    }
+}
 
 console.log(`Listening on ${server.url}`)
--- backend/src/types.ts
+++ backend/src/types.ts
@@ -7,3 +7,8 @@ export type Spot = {
       longitude: number
       createdAt: string
     }
+
+export type SpotError = {
+  ok: false
+  message: "invalid body" | "title missing" | "category missing" | "description missing" | "latitude missing" | "longitude missing" | "latitude invalid" | "longitude invalid" | "category invalid" | "title invalid" | "description invalid"
+}
